d494c0c45368fa8728382f3ab2620b4b6d8bbd24
oumy42010 <diatta.oumy@yahoo.com>
2017-03-02 15:33:33 +0100

    Update cv.js

--- cv.js
+++ cv.js
@@ -3,3 +3,16 @@
 	function afficher(id) {
 		$(id).toggle();
 	}
+
+
+	function valider(){
+		console.log($("#email").val().includes("@"));
+		if(!$("#email").val().includes("@")){
+			window.alert("Adresse mail est incorrect, veuillez rettaper!");
+		}
+		
+		console.log($("#phone").val().length);
+		if($("#phone").val().length != 10){
+			window.alert("Le numéro de téléphone doit avoir 10 chiffres");	
+		}
+	}
